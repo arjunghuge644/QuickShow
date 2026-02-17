0f42043d1b67fff68294d39c3792b1bae3fadd7e
Arjun Ghuge <arjunghuge644@gmail.com>
2026-02-17 10:14:38 +0530

    first commit

---
+++ client/src/components/HeroSection.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HeroSection = () => {
+  return (
+        <div className='flex flex-col itens-start justify-center '>
+
+        </div> 
+
+    );
+
+};
+
+export default HeroSection;
