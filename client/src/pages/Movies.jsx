a1e9075eb7375522dd3547cf2168b8dabd61f36e
Arjun Ghuge <arjunghuge644@gmail.com>
2026-02-21 16:34:36 +0530

    moviedetails done

--- client/src/pages/Movies.jsx
+++ client/src/pages/Movies.jsx
@@ -17,7 +17,9 @@ const Movies = () => {
       </div>
     </div>
   ) : (
-    <div></div>
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-3xl font-bold text-center">No movies available</h1>
+    </div>
   );
 };
 
--- client/src/pages/favorite.jsx
+++ client/src/pages/favorite.jsx
@@ -1,10 +1,26 @@
 import React from "react";
+import { dummyShowsData } from "../assets/assets";
+import MovieCard from "../components/MovieCard";
+import BlurCircle from "../components/BlurCircle";
 
 const Favorite = () => {
-  return (
-    <div>       
+  return dummyShowsData.length > 0 ? (
+    <div className="relative my-20 px-6 md:px-16 lg:px-24 xl:px-32 overflow-hidden min-h-[80vh]">
+      <BlurCircle top="150px" left="0px" />
+      <BlurCircle bottom="150px" right="50px" />
 
-    </div>  );
-}   
+      <h1 className="text-lg font-medium my-4">Your Favorite Movies</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+        {dummyShowsData.map((movie) => (
+          <MovieCard movie={movie} key={movie._id} />
+        ))}
+      </div>
+    </div>
+  ) : (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-3xl font-bold text-center">No movies available</h1>
+    </div>
+  );
+};
 
 export default Favorite;
